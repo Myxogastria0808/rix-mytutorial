5335c6db517a292e5e49329606a417a78a894781
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-09-20 14:06:01 +0900

    initial commit

---
+++ env.r
@@ -0,0 +1,13 @@
+library(rix)
+
+rix(
+    r_ver = "latest",
+    r_pkgs = c("dplyr", "ggplot2"),
+    system_pkgs = NULL,
+    git_pkgs = NULL,
+    ide = "code",
+    shell_hook = NULL,
+    project_path = "~/Desktop/rix-mytutorial/",
+    overwrite = TRUE,
+    print = TRUE
+)
